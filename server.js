95f953a237d2d8b996b1a4e48b389f95c12ef309
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2023-06-30 20:19:28 +0530

    server created

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+require("dotenv").config({});
+const connectDB = require("./database/db");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+//route paths
+
+// Call the connectDB function to connect to the database
+connectDB();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    limit: "100mb",
+    extended: false,
+  })
+);
+app.use(cors());
+app.use(express.json());
+
+app.get("/get", (req, res) => {
+  res.send("E-commerce Server");
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
+
+//Routes middleware
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
